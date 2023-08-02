10ca10374d867e43f68363edf8eccd496ba00977
HossainMaruf <maruficepustian@gmail.com>
2023-08-02 22:46:27 +0600

    ejs engine rendering

--- server.js
+++ server.js
@@ -3,24 +3,42 @@ const path = require('path')
 const app = express()
 
 
+// select the view directory for our app
+app.set('views', 'views');
+// setting the templating engine
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'views', "Home.html"));
+  const data = {
+    pageTitle: "Home",
+    heading: "SOMETHING ABOUT HOME"
+  }
+  res.render('Home', {data});
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', "About.html"));
+  const data = {
+    pageTitle: "About",
+    heading: "About Page"
+  }
+  res.render('About', {data});
 })
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', "Contact.html"));
+  const data = {
+    pageTitle: "Contact",
+    heading: "Contact Page"
+  }
+  res.render('Contact', {data});
 })
 
 app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', "Dashboard.html"));
+  res.render('Dashboard');
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', "404.html"));
+  res.render('404');
 })
 
 
